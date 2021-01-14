cfc47a65f54994f7952b608dfc586b0f741d613f
Alec Scully <ScullyAlec@gmail.com>
2021-01-13 23:42:26 -0500

    commit tree 9b0603c682f3dd958711951a0b9d706eff152b83

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, string){
+    let notes = []
+    for (let i = 0; i < array.length; i++) {
+        notes.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`)
+    }
+
+    return notes
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int)
+        int--
+    }
+}
